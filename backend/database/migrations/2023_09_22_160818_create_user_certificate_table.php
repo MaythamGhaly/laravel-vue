949185f782af621002d8ac430008897d2b406e26
maythamghaly <maythamghaly@gmail.com>
2023-09-22 19:18:09 +0300

    add migrations

---
+++ backend/database/migrations/2023_09_22_160818_create_user_certificate_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('user_certificate', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->bigInteger('certificate_id')->unsigned();
+            $table->foreign('certificate_id')->references('id')->on('certificates')->onDelete('cascade');
+            $table->string('level');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('user_certificate');
+    }
+};
